fd80c14729892add0b3c9abaf28f8d75dd7e5bc2
Alberto Vilches <vilches@gmail.com>
2019-03-13 23:43:51 +0100

    first commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    public RawImage background;
+    public float speed = 0.2F;
+    public Canvas startTitle;
+    
+
+    public enum State {
+        idle,
+        playing
+    }
+
+    public State state;
+
+    // Start is called before the first frame update
+    void Start() {
+        state = State.idle;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (state == State.idle) {
+            startTitle.gameObject.SetActive(true);
+
+            if (Input.GetKey(KeyCode.Space)) {
+                StartGame();
+            }
+        } else {
+            startTitle.gameObject.SetActive(false);
+            Parallax();
+            
+            if (Input.GetKey(KeyCode.Escape)) {
+                state = State.idle;
+            }
+
+        }
+    }
+
+    private void StartGame() {
+        state = State.playing;
+    }
+
+    private void Parallax() {
+        background.uvRect = new Rect(background.uvRect.x + speed * Time.deltaTime, 0f, 1f, 1f);
+    }
+}
